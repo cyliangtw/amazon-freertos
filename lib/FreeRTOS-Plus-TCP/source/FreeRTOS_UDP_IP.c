fe5f7ee256820d8964a42650513ffaf683dd5563
cyliangtw <cyliang@nuvoton.com>
2018-08-09 15:24:20 +0800

    Fixed wrong constant var. declaration for memcpy's destination

--- lib/FreeRTOS-Plus-TCP/source/FreeRTOS_UDP_IP.c
+++ lib/FreeRTOS-Plus-TCP/source/FreeRTOS_UDP_IP.c
@@ -136,7 +136,7 @@ uint32_t ulIPAddress = pxNetworkBuffer->ulIPAddress;
 			 * Offset the memcpy by the size of a MAC address to start at the packet's
 			 * Ethernet header 'source' MAC address; the preceding 'destination' should not be altered.
 			 */
-			const char *pxUdpSrcAddrOffset = ( ( char *) pxUDPPacket + sizeof( MACAddress_t ) );
+			char *pxUdpSrcAddrOffset = ( ( char *) pxUDPPacket + sizeof( MACAddress_t ) );
 			memcpy( pxUdpSrcAddrOffset, xDefaultPartUDPPacketHeader.ucBytes, sizeof( xDefaultPartUDPPacketHeader ) );
 
 		#if ipconfigSUPPORT_OUTGOING_PINGS == 1
